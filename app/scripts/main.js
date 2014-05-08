e537e11cac9c73e9e83a4dd643a99f93cd6feece
João Netto <hi@joaonetto.me>
2014-05-07 21:07:48 -0300

    Adding static content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+$(window).load( function() {
+
+    $('#container').masonry({
+        "itemSelector": ".item",
+        "columnWidth": ".grid-sizer",
+    });
+
+});
