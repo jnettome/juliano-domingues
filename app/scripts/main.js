98af33aa6eaa5b24f4009d77e1aff0a18fa052de
João Netto <hi@joaonetto.me>
2014-06-20 17:50:32 -0300

    Fixing jobs location

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,16 @@ $(function() {
         $(this).css('background-image', 'url('+$(this).data('background')+')');
     });
 
+    $('.background-image').on('mouseenter', function() {
+        $(this).css('background-image', 'url('+$(this).data('background-hover')+')');
+    });
+
+    $('.background-image').on('mouseleave', function() {
+        $(this).css('background-image', 'url('+$(this).data('background')+')');
+    });
+});
+
+$(window).on('resize load', function() {
+    $('.resizr').css('width', $('.background-image').outerWidth());
+    $('.resizr').css('height', $('.background-image').outerHeight());
 });
