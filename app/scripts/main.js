aa45af42b188aa561e1b01ab32cf727ea44cd1ba
João Netto <hi@joaonetto.me>
2014-07-12 20:45:24 -0300

    Removing animate.css, adding more works to fill grid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,12 +14,3 @@ $(function() {
         $(this).css('background-image', 'url('+$(this).data('background')+')');
     });
 });
-
-// Animation
-$(document).ready(function() {
-    var finishEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    $('.headline').addClass('animated fadeInDown').one(finishEvents, function() {
-        $('.site-navigation').addClass('animated fadeIn');
-        $('.scroll-holder').addClass('animated fadeIn');
-    });
-});
