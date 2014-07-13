a8292c0e0c02c37a6f58174a48ad821469e6a8da
João Netto <hi@joaonetto.me>
2014-07-12 21:13:52 -0300

    Adding images

--- Gruntfile.js
+++ Gruntfile.js
@@ -241,7 +241,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= config.app %>/images',
-                    src: '{,*/}*.{gif,jpeg,jpg,png}',
+                    src: '{,*/}*.{gif,jpeg,jpg}',
                     dest: '<%= config.dist %>/images'
                 }]
             }
